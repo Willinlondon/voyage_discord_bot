7a6d091a1d3a5e795d4a662b8e3bd6610e009429
Christopher Webb <31657038+cwebbtw@users.noreply.github.com>
2020-12-06 13:14:42 +0000

    Update bot_wow_api.py

--- bot_wow_api.py
+++ bot_wow_api.py
@@ -4,7 +4,7 @@ import wowapi
 
 wow_api_client = wowapi.WowApi(os.getenv('BLIZZARD_CLIENT_ID'), os.getenv('BLIZZARD_CLIENT_SECRET'))
 
-identify_api_call_pattern = re.compile("\${\wow_api\.([a-zA-Z_-].+)\(([0-9,]+)\)\}")
+identify_api_call_pattern = re.compile("\${\wow_api\.([a-zA-Z_-].+)\(([0-9,]+)?\)\}")
 
 def fetch_ranks(args):
     rank_list = args[0]
