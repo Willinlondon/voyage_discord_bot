85d757d103a42b8d916fe5064714703cd90c69b8
Christopher Webb <31657038+cwebbtw@users.noreply.github.com>
2020-12-07 23:14:23 +0000

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -10,3 +10,11 @@ DISCORD_OFFICER_CHANNEL = config.get('Discord', 'officer.channel')
 GAME_GUILD = config.get('Game', 'guild')
 GAME_REALM = config.get('Game', 'realm')
 GAME_REGION = config.get('Game', 'region')
+
+print(f"Loaded: DISCORD_APPLICATION_CHANNEL={DISCORD_APPLICATION_CHANNEL}")
+print(f"Loaded: DISCORD_GUILD={DISCORD_GUILD}")
+print(f"Loaded: DISCORD_OFFICER_CHANNEL={DISCORD_OFFICER_CHANNEL}")
+
+print(f"Loaded: GAME_GUILD={GAME_GUILD}")
+print(f"Loaded: GAME_REALM={GAME_REALM}")
+print(f"Loaded: GAME_REGION={GAME_REGION}")
