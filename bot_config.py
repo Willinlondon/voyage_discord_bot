ef21a8564a76daa90fc4af111c74480f39e0c57b
Christopher Webb <31657038+cwebbtw@users.noreply.github.com>
2020-12-07 23:21:36 +0000

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -3,9 +3,9 @@ import configparser
 config = configparser.RawConfigParser()
 config.read('config/config.properties')
 
-DISCORD_APPLICATION_CHANNEL = config.get('Discord', 'applications.channel')
-DISCORD_GUILD = config.get('Discord', 'guild')
-DISCORD_OFFICER_CHANNEL = config.get('Discord', 'officer.channel')
+DISCORD_APPLICATION_CHANNEL = config.getint('Discord', 'applications.channel')
+DISCORD_GUILD = config.getint('Discord', 'guild')
+DISCORD_OFFICER_CHANNEL = config.getint('Discord', 'officer.channel')
 
 GAME_GUILD = config.get('Game', 'guild')
 GAME_REALM = config.get('Game', 'realm')
