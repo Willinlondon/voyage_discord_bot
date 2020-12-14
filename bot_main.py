5c6392fe852cecb8c7c35293c3e77104bf2df92b
Christopher Webb <31657038+cwebbtw@users.noreply.github.com>
2020-12-14 19:07:18 +0000

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -24,9 +24,7 @@ async def on_member_join(member):
     
 @bot.event
 async def on_message(message):
-    print(message.author)
-    print(message)
-    if message.author == 236585733341708290:
+    if message.author.id == 236585733341708290:
         channel = bot.get_channel(788037136003104778)
         await channel.send('and the trumpets they go!')
     else:
